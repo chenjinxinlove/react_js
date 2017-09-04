14df40a1a6a6f38fd17814d23ee2abf6446422ea
陈金鑫 <505150704@qq.com>
2017-09-04 17:12:07 +0800

    get set

---
+++ Object.js
@@ -0,0 +1,28 @@
+var person = {};
+Object.defineProperty(person, "name", {
+    writable: false,
+    value: 'CHEN'
+});
+console.log(person.name);//CHEN
+person.name = 'jin';
+console.log(person.name);//CHEN
+
+var book = {
+    _year : 2004,
+    edition: 1
+};
+
+Object.defineProperty(book, "year", {
+    get: function () {
+        return this._year;
+    },
+    set: function (newValue) {
+        if (newValue > 2004) {
+            this._year = newValue;
+            this.edition += newValue - 2004;
+        }
+    }
+})
+
+book.year = 2019;
+console.log(book);
